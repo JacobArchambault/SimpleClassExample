c7f802d15da58af9f5ac6500217423935e8bebbc
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 08:56:00 -0400

    Add master constructor to Motorcycle class.

--- SimpleClassExample/Motorcycle.cs
+++ SimpleClassExample/Motorcycle.cs
@@ -9,18 +9,25 @@ namespace SimpleClassExample
     class Motorcycle
     {
         public int driverIntensity;
-        public string name;
+        public string driverName;
 
         // Put back the default constructor, which will set all data members to default values.
         public Motorcycle() { }
-
-        // Our custom constructor.
         public Motorcycle(int intensity)
+            : this(intensity, "") { }
+        public Motorcycle(string name)
+            : this(0, name) { }
+        public Motorcycle(int intensity, string name)
         {
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
             driverIntensity = intensity;
+            driverName = name;
         }
 
-        public void SetDriverName(string Name)
+        public void SetDriverName(string name)
         {
             this.name = name;
         }
